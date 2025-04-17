581fc17921d2864492ce725e8faa0f127bd4ac7f
gabrielrez <gabrielrezcpessoa@gmail.com>
2025-04-17 09:50:32 -0300

    style: hover no botão de login

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro</title>
+    <title>Login</title>
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -20,14 +20,14 @@
         <form action="/login" method="POST" class="mt-16 max-w-[360px] w-xs">
             <div>
                 <label for="email" class="block">Email</label>
-                <input type="email" name="email" class="bg-[#F5F5F5] border border-[#D9D9D9] px-2 py-2 w-full mt-1.5 rounded-2xl" placeholder="exemplo@email.com">
+                <input type="email" name="email" class="bg-[#F5F5F5] border border-[#D9D9D9] px-2 py-2 w-full mt-1.5 rounded-2xl" placeholder="exemplo@email.com" required>
             </div>
             <div class="mt-5">
                 <label for="password" class="block">Senha</label>
-                <input type="password" class="bg-[#F5F5F5] border border-[#D9D9D9] px-2 py-2 w-full mt-1.5 rounded-2xl" name="password" placeholder="************">
+                <input type="password" class="bg-[#F5F5F5] border border-[#D9D9D9] px-2 py-2 w-full mt-1.5 rounded-2xl" name="password" placeholder="************" required>
             </div>
             <div class="mt-10">
-                <button type="submit" class="bg-[#E76F51] cursor-pointer block w-full py-2 rounded-full text-white font-semibold">Entrar na conta</button>
+                <button type="submit" class="bg-[#E76F51] cursor-pointer block w-full py-2 rounded-full text-white font-semibold hover:bg-[#D65F43] transition-all duration-150 ease-in-out">Entrar na conta</button>
                 <p class="text-sm text-center mt-5">Ainda não tem uma conta? <a href="/register" class="font-semibold underline">Criar Conta</a></p>
             </div>
         </form>
