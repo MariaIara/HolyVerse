b3aa906f1a52105535a2b26d42101a8487b28170
MariaIara <mariaiarasbraga@gmail.com>
2025-04-18 18:59:21 -0300

    feat: implementação de métodos de cadastro e login

---
+++ app/Views/app/home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Oi chaminho</h1>
+    <form action="/logout" method="POST">
+        <button type="submit">Sair da Conta</button>
+    </form>
+</body>
+</html>
